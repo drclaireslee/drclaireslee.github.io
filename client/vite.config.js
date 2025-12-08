65e66409bac2915c2f4bd3b148190a595ff35fb5
AmazingConnie <rmallu21@gmail.com>
2025-12-08 15:47:07 -0500

    change vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://drclaireslee.vercel.app",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api")
-      }
-    }
-  }
 });
